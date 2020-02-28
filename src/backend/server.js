ff9b8b60facc9797c1bf8e2856117205c1d840b5
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-02-28 23:47:44 +0530

    Basic JWT authentication

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,29 +1,32 @@
-var express = require("express")
-var cors = require("cors")
-var bodyParser = require("body-parser")
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
 var app = express()
-var mongoose = require("mongoose")
+const mongoose = require('mongoose')
 var port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(cors())
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 )
 
-const mongoURI = 'mongodb://localhost:27017/KRB'
+const mongoURI = 'mongodb://localhost:27017/KRBApplication'
 
 mongoose
-    .connect(mongoURI, {useNewUrlParser: true})
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err))
+  .connect(
+    mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
 
 var Users = require('./routes/Users')
 
 app.use('/users', Users)
 
-app.listen(port, () => {
-    console.log("Server is running on port: " + port)
+app.listen(port, function() {
+  console.log('Server is running on port: ' + port)
 })
