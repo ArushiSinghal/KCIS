3d190cc252400ec078b7f93e11763778921be8da
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-04-21 01:46:10 +0530

    Cleaned

--- zip/src/BACnet/checktemp.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("sshpass -p 'mouni1995' ssh 'lingam@10.2.24.202' 'cd pybacnet;cd tools;python checktemp.py' ")
-
--- zip/src/react-material-backend/routes/checkstatus.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("sshpass -p 'mouni1995' ssh 'user@10.2.24.157' 'cd pybacnet;cd tools;python checkstatus.py' ")
-
